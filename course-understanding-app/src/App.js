ed8fdba8341c91530769066aa91b807210e0e291
Aryan Yalavarthi <2300030049@kluniversity.in>
2024-08-21 22:58:04 +0530

    Deleted the LOGO.svg and used the map function to print an array of numbers using the item module

--- course-understanding-app/src/App.js
+++ course-understanding-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Kamran";
+  let age = 25;
+  let l = [1,2,3,4,5,6,7,8,9,10];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
+      {l.map((item) => {
+        return (
+          <div>
+            {item}
+          </div>
+        )
+      })}  
     </div>
   );
 }
